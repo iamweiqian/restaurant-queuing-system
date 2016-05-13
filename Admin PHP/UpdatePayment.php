39620ed8d2594f0184236c37b127d2a07de9f7fe
Wei Qian <wqyap762@hotmail.com>
2016-05-13 10:57:52 +0800

    Added phone number to the order list.
    1. Added phone number to the order list for admin site.
    2. Centered today’s sales chart.

--- Admin PHP/UpdatePayment.php
+++ Admin PHP/UpdatePayment.php
@@ -26,6 +26,7 @@
 			<th width="48">No.</th>
 			<th width="82">Order ID</th>
 			<th width="120">Customer's Name</th>
+			<th width="100">Phone No.</th>
 			<th width="196">Food Name</th>
 			<th width="83">Quantity</th>
 			<th width="104">Total Price (RM)</th>
@@ -38,7 +39,7 @@
     		require "init.php";
     		$order_id = $_POST['order_id'];
 
-   			$sql = "SELECT o.*, u.name, m.food_name FROM `Order` o INNER JOIN `User` u ON o.FK_hpno = u.hpno INNER JOIN `Menu` m ON o.FK_menu_id = m.menu_id WHERE order_id LIKE '$order_id'";
+   			$sql = "SELECT o.*, u.hpno, u.name, m.food_name FROM `Order` o INNER JOIN `User` u ON o.FK_hpno = u.hpno INNER JOIN `Menu` m ON o.FK_menu_id = m.menu_id WHERE order_id LIKE '$order_id'";
    			$result = mysqli_query($con, $sql) or die("Error: ".mysqli_error($con));
 
     		while ($row = mysqli_fetch_array($result)) {
@@ -48,6 +49,7 @@
           			<td><?php echo $i;?></td>
           			<td><?php echo $row{'order_id'};?></td>
           			<td><?php echo $row{'name'};?></td>
+          			<td><?php echo $row{'hpno'};?></td>
           			<td style="text-align:left"><?php echo $row{'food_name'};?></td>
           			<td><?php echo $row{'quantity'};?></td>
           			<td><?php echo $row{'total_price'};?></td>
--- Admin PHP/index.php
+++ Admin PHP/index.php
@@ -1,8 +1,13 @@
+<?php
+	$page = $_SERVER['PHP_SELF'];
+	$sec = "10";
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Restaurant Parit Raja Management System</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+  <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
 </head>
 
 <body>
@@ -27,11 +32,12 @@
 		<tr>
 			<th width="40">No.</th>
 			<th width="74">Order ID</th>
-			<th width="118">Customer's Name</th>
+			<th width="120">Customer's Name</th>
+			<th width="100">Phone No.</th>
 			<th width="182">Food Name</th>
 			<th width="75">Quantity</th>
 			<th width="96">Total Price (RM)</th>
-      <th width="79">Order Time</th>
+      <th width="100">Order Time</th>
 			<th width="93">Payment Status</th>
 		</tr>
 		<?php
@@ -39,7 +45,7 @@
     		require "init.php";
     		$order_id = $_POST['order_id'];
 
-   			$sql = "SELECT o.*, u.name, m.food_name FROM `Order` o INNER JOIN `User` u ON o.FK_hpno = u.hpno INNER JOIN `Menu` m ON o.FK_menu_id = m.menu_id WHERE DATE(`ordered_on`) = CURDATE()";
+   			$sql = "SELECT o.*, u.hpno, u.name, m.food_name FROM `Order` o INNER JOIN `User` u ON o.FK_hpno = u.hpno INNER JOIN `Menu` m ON o.FK_menu_id = m.menu_id WHERE DATE(`ordered_on`) = CURDATE() ORDER BY order_id DESC ";
    			$result = mysqli_query($con, $sql) or die("Error: ".mysqli_error($con));
 
     		while ($row = mysqli_fetch_array($result)) {
@@ -49,11 +55,12 @@
           			<td><?php echo $i;?></td>
           			<td><?php echo $row{'order_id'};?></td>
           			<td><?php echo $row{'name'};?></td>
+          			<td><?php echo $row{'hpno'};?></td>
           			<td style="text-align:left"><?php echo $row{'food_name'};?></td>
           			<td><?php echo $row{'quantity'};?></td>
           			<td><?php echo $row{'total_price'};?></td>
           			<td><?php echo $row{'ordered_on'};?></td>
-					<td><?php echo $row{'payment_status'};?></td>
+								<td><?php echo $row{'payment_status'};?></td>
         		</tr>
     			<?php }
 		?>
